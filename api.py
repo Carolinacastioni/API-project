7a6220a1a7e07feff49f43ea4ff3e88cd7413195
Carolina Castioni <carolinapcastioni@gmail.com>
2020-04-30 20:32:33 +0200

    estructura done

--- api.py
+++ api.py
@@ -1,9 +1,13 @@
 from src.config import PORT
 from src.app import app
 from flask import Flask, request
+import src.mongo 
+import src.errorHandler
 
 @app.route("/hola")
 def basicResponse():
     return 'Hello, World!'
+
+
 
 app.run("0.0.0.0", PORT, debug=True)
--- src/mongo.py
+++ src/mongo.py
@@ -2,18 +2,24 @@ from pymongo import MongoClient
 from src.config import DBURL
 from bson.json_util import dumps
 from src.errorHandler import errorHandler, Error404
+from flask import Flask, request
+from src.app import app
+import re
 
 client = MongoClient(DBURL)
 print(f"Connected to {DBURL}")
-db = client.get_default_database()["api-project.quotes"]
+db = client.get_default_database()["quotes"]
 
-@app.route("/chat/<id>")
+@app.route("/user/<name>")
 @errorHandler
-def getChat(id):
-    id = db.find({"chat":id},{"_id":0, "user":1, "quote":1, "chat":1})
-    print(f"chat: {id}")
-    if not id:
+def getUser(name):
+    namereg = re.compile(f"^{name}", re.IGNORECASE)
+    user = db.find({"user":namereg},{"_id":0, "user":1, "quote":1, "chat":1})
+    print(f"user: {namereg}")
+    x = dumps(user)
+    if len(x) < 3:
         print("ERROR")
-        raise Error404("chat not found")
+        raise Error404("user not found")
     print("OK")
-    return dumps(id)
+    print(len(x))
+    return x
